1ecf1d3366542512c988a8301040810a253ecd90
Geoffrey Makawa <geoffreymakawa04@gmail.com>
2022-10-19 18:20:30 +0200

    Announcement entity bootstrapped

---
+++ src/main/java/com/groupnumber8/onlinenoticeboard/entities/Announcement.java
@@ -0,0 +1,27 @@
+package com.groupnumber8.onlinenoticeboard.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Announcement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String message;
+    private Date postedOn;
+    private Date expireOn;
+}
