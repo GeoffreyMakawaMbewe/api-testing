d8db827f1e0a3fa3346e4c4993f7be7c4ab8bba6
Geoffrey Makawa <geoffreymakawa04@gmail.com>
2022-10-19 18:22:27 +0200

    Announcement entity bootstrapped

---
+++ src/main/java/com/groupnumber8/onlinenoticeboard/repository/AnnouncementRepository.java
@@ -0,0 +1,10 @@
+package com.groupnumber8.onlinenoticeboard.repository;
+
+import com.groupnumber8.onlinenoticeboard.entities.Announcement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
+    Optional<Announcement> findAnnouncementByName(String name);
+}
