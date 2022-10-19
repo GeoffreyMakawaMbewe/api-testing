06dd61ae605462171e9e5fa534a9bc62e83807a4
Geoffrey Makawa <geoffreymakawa04@gmail.com>
2022-10-19 17:53:52 +0200

    Project template

---
+++ src/main/java/com/groupnumber8/onlinenoticeboard/OnlineNoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.groupnumber8.onlinenoticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineNoticeBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineNoticeBoardApplication.class, args);
+    }
+
+}
