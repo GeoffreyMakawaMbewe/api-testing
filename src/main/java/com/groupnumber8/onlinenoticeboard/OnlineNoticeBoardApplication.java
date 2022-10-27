4bd673eeaa9cc41d1ba2b1d41e2810ed3c02b754
Geoffrey Makawa <bed-com-36-18@unima.ac.mw>
2022-10-27 05:25:48 -0700

    Started Student class attributes

--- src/main/java/com/groupnumber8/onlinenoticeboard/OnlineNoticeBoardApplication.java
+++ src/main/java/com/groupnumber8/onlinenoticeboard/OnlineNoticeBoardApplication.java
@@ -1,12 +1,12 @@
 package com.groupnumber8.onlinenoticeboard;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Date;
 
-@OpenAPIDefinition
+//@OpenAPIDefinition
 @SpringBootApplication
 public class OnlineNoticeBoardApplication {
 
---
+++ src/main/java/com/groupnumber8/onlinenoticeboard/users/config/ApplicationConfig.java
@@ -0,0 +1,44 @@
+package com.groupnumber8.onlinenoticeboard.users.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/postNewAnnouncement").hasAnyRole("STUDENT","ADMIN")
+                .mvcMatchers("/announcement/{name}").hasRole("ADMIN")
+                .and()
+                .httpBasic();
+
+
+
+
+    }
+}
