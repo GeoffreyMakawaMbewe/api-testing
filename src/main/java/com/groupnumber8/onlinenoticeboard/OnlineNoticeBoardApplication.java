ab0055d2a34b2dec5206f2a7002d828f23fb1945
Geoffrey Makawa <bed-com-36-18@unima.ac.mw>
2022-10-29 09:38:13 -0700

    Deleted and resting using new approach, for all unworking classes

--- src/main/java/com/groupnumber8/onlinenoticeboard/OnlineNoticeBoardApplication.java
+++ src/main/java/com/groupnumber8/onlinenoticeboard/OnlineNoticeBoardApplication.java
@@ -1,10 +1,9 @@
 package com.groupnumber8.onlinenoticeboard;
 
 //import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Date;
 
 //@OpenAPIDefinition
 @SpringBootApplication
@@ -12,8 +11,6 @@ public class OnlineNoticeBoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineNoticeBoardApplication.class, args);
-
-
     }
 
 }
